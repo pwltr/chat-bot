465df4b91dc00f4a015454ff41792530668c4176
Philipp Walter <philippwalter@pm.me>
2021-08-16 21:51:51 +0200

    fix Mui & styled-components SSR problems

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     '^@src(.*)$': '<rootDir>/src$1',
     '^@components(.*)$': '<rootDir>/src/components$1',
     '^@pages(.*)$': '<rootDir>/src/pages$1',
+    '^@services(.*)$': '<rootDir>/src/services$1',
   },
 }
