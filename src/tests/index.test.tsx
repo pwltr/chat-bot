fd630f620c488396941f96c84da5f1bdefcae2e4
Philipp Walter <philippwalter@pm.me>
2021-08-17 10:16:49 +0200

    add some tests

--- src/tests/index.test.tsx
+++ src/tests/index.test.tsx
@@ -1,123 +1,12 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
+import ConversationService from '@services/api/conversation'
 import Home from '@pages/index'
+import { steps } from './mockData'
 
-type TOption = {
-  nextId: number
-  value: number | string | boolean
-  text: string
-}
-
-type TStep = {
-  id: number
-  name: string
-  text: string
-  uiType: 'button'
-  valueType: 'number' | 'string' | 'boolean'
-  valueOptions: TOption[]
-}
-
-
-// TODO: mock fetch and get mock data
-const steps = [
-  {
-    id: 100,
-    name: 'liability',
-    text: 'Benötigen Sie eine Haftplichtversicherung?',
-    uiType: 'button',
-    valueType: 'boolean',
-    valueOptions: [
-      {
-        nextId: 200,
-        value: true,
-        text: 'Ja',
-      },
-      {
-        nextId: 200,
-        value: false,
-        text: 'Nein',
-      },
-    ],
-  },
-  {
-    id: 200,
-    name: 'casco',
-    text: 'Benötigen Sie eine Kasko?',
-    uiType: 'button',
-    valueType: 'boolean',
-    valueOptions: [
-      {
-        nextId: 201,
-        value: true,
-        text: 'Ja',
-      },
-      {
-        nextId: 300,
-        value: false,
-        text: 'Nein',
-      },
-    ],
-  },
-  {
-    id: 201,
-    name: 'cascoType',
-    text: 'Welche Art von Kasko benötigen Sie?',
-    uiType: 'button',
-    valueType: 'string',
-    valueOptions: [
-      {
-        nextId: 300,
-        value: 'full',
-        text: 'Vollkasko',
-      },
-      {
-        nextId: 300,
-        value: 'part',
-        text: 'Teilkasko',
-      },
-    ],
-  },
-  {
-    id: 300,
-    name: 'licensePlateType',
-    text: 'Welche Kennzeichenart benötigen Sie?',
-    uiType: 'button',
-    valueType: 'string',
-    valueOptions: [
-      {
-        nextId: false,
-        value: 'ekz',
-        text: 'Einzelkennzeichen',
-      },
-      {
-        nextId: 301,
-        value: 'wkz',
-        text: 'Wechselkennzeichen',
-      },
-    ],
-  },
-  {
-    id: 301,
-    name: 'vehicles',
-    text: 'Wieviele Fahrzeuge wollen Sie versichern?',
-    uiType: 'button',
-    valueType: 'number',
-    valueOptions: [
-      {
-        nextId: false,
-        value: 2,
-        text: 'Zwei Fahrzeuge',
-      },
-      {
-        nextId: false,
-        value: 3,
-        text: 'Drei Fahrzeuge',
-      },
-    ],
-  },
-] as TStep[]
+jest.mock('@services/api/conversation')
 
 describe('<Home /> spec', () => {
   it('renders the correct question', () => {
@@ -125,12 +14,35 @@ describe('<Home /> spec', () => {
     expect(screen.getByText('Benötigen Sie eine Haftplichtversicherung?')).toBeInTheDocument()
   })
 
-  // it('renders the correct question after click', async () => {
-  //   render(<Home steps={steps} />)
+  it('renders the correct question after click', async () => {
+    render(<Home steps={steps} />)
 
-  //   const button = await screen.findByText('Ja')
-  //   button.click()
+    const button = await screen.getByTestId('button-liability-Ja')
+    button.click()
 
-  //   expect(screen.findByText('Benötigen Sie eine Kasko?')).toBeInTheDocument()
-  // })
+    expect(screen.getByText('Benötigen Sie eine Kasko?')).toBeInTheDocument()
+  })
+
+  it('renders thank you message and submits the correct data', async () => {
+    render(<Home steps={steps} />)
+
+    const button1 = await screen.getByTestId('button-liability-Ja')
+    button1.click()
+
+    const button2 = await screen.getByTestId('button-casco-Nein')
+    button2.click()
+
+    const button3 = await screen.getByTestId('button-licensePlateType-Einzelkennzeichen')
+    button3.click()
+
+    expect(screen.getByText('Herzlichen Dank für Ihre Angaben!')).toBeInTheDocument()
+
+    await waitFor(() => expect(ConversationService.sendAnswers).toHaveBeenCalledTimes(1))
+
+    expect(ConversationService.sendAnswers).toHaveBeenCalledWith([
+      { name: 'liability', value: true },
+      { name: 'casco', value: false },
+      { name: 'licensePlateType', value: 'ekz' },
+    ])
+  })
 })
